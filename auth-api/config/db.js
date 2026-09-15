3f0aa9f4dbdc73cc47161106d1f358030517833b
Ivan Ruiz <ivanjoseph.ruizbarajas@gmail.com>
2026-09-15 16:08:10 -0500

    fix: config db.js with env vars and dotenv

--- auth-api/config/db.js
+++ auth-api/config/db.js
@@ -1,18 +1,15 @@
-// auth-api/config/db.js
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-// Crear el Pool de conexiones
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',          
-    password: 'IvanRB84276',          
-    database: 'sander_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT),
+    ssl: { rejectUnauthorized: false }, 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// Promisificar el pool para usar async/await
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = pool;
